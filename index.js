2b59adbc5f9fb1705eea72c8590d5263b337c55f
Jullian Cockerell <jullian.cockerell@yahoo.com>
2018-03-01 21:44:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments)
 {
   var empty = [];
   var c = 0;
-  while(musicians.length > 0)
+  while(c < musicians.length)
   {
     empty.push(`${musicians[c]} plays ${instruments[c]}`);
     c = c + 1;
