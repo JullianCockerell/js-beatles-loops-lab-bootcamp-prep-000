8a0c3c604cbf08a564796ddac62e545769e1afd8
Jullian Cockerell <jullian.cockerell@yahoo.com>
2018-03-01 21:42:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function theBeatlesPlay(musicians, instruments)
+{
+  var empty = [];
+  var c = 0;
+  while(musicians.length > 0)
+  {
+    empty.push(`${musicians[c]} plays ${instruments[c]}`);
+    c = c + 1;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
